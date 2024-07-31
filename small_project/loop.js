2bb598eb5facd4279ff5322cde9c36bf3e79c02c
2705jagriti <jagritichaudhary842@gmail.com>
2024-07-31 20:54:40 +0530

    project

---
+++ small_project/loop.js
@@ -0,0 +1,51 @@
+// console.log("This is loop tutorial")
+// let a = 1;
+// for (let i = 0; i < 100; i++) {
+//     console.log( a + i );
+
+// }   
+// let obj = {
+//     fname:"jagriti",
+//     age :21,
+//     education :"srmcem"
+// }
+// for (const key in obj) {
+//         const element = obj[key];
+//        console.log (key,element)
+// //     }
+//  let userName = "hary bhai"
+//  let newUserName = userName.replace("bhai" , "bhau")
+//  console.log(newUserName)
+// let arr =[1,2,3,4,5,6]
+// arr[0]=6
+// console.log(arr)
+// arr.splice(1,0,34 ,65);
+// arr.push(73)
+//  let = arr.slice(1,2);
+// console.log(arr)
+// console.log(arr.length)
+// const  arr =[1,2,3,4,5,6]
+// for (const num of arr) {
+//     console.log(arr)
+    
+// }
+// const greeting = "Hello world"
+// for (const greet of greeting) {
+//     console.log(`each char id ${greet}`)
+// }
+// //maps
+// const map = new Map()
+// map
+
+// console.error("this is an error")]\
+//can or cant drive
+// let age =prompt("Enter your age")
+// if(age>18){
+//     alert("Can drive")
+// }
+// else{
+//     alert("Can't drive")
+// }
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+// boxes[2].style.backgroundColor="green"
