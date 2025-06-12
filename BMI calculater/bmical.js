68f6e57136f179716da43b2c64f0386c756889e1
2705jagriti <jagritichaudhary842@gmail.com>
2025-06-12 19:08:17 +0530

    ..

--- BMI calculater/bmical.js
+++ BMI calculater/bmical.js
@@ -1,23 +1,20 @@
-let form = document.querySelector('form')
-form.addEventListener('submit' , function(e){ 
-e.preventDefault();
+let form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-let height = parseInt(document.querySelector('#height').value);
-let weight = parseInt(document.querySelector('#weight').value);
-let results = document.querySelector('#results');
+  let height = parseInt(document.querySelector("#height").value);
+  let weight = parseInt(document.querySelector("#weight").value);
+  let results = document.querySelector("#results");
 
-
-if( height==='' || height < 0|| isNaN(height)){
+  if (height === "" || height < 0 || isNaN(height)) {
     results.innerHTML = `Please give a valid height ${height}`;
-}
-else if( weight='' || weight<0|| isNaN(weight))
-{    results.innerHTML = `Please give a valid height ${height}`;}
-
-else{
-const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-results.innerHTML= `<span>${bmi}</span> ` ; }
-})
-
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    results.innerHTML = `<span>${bmi}</span>  `;
+  }
+});
 
 // const form = document.querySelector('form');
 // form.addEventListener('submit', function (e) {
@@ -36,4 +33,3 @@ results.innerHTML= `<span>${bmi}</span> ` ; }
 //     results.innerHTML = `<span>${bmi}</span>`;
 //   }
 // });
-
